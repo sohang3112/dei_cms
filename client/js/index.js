147296988c29da5ba0da8cfbf9cf59b248f0b709
Sohang Chopra <sohangchopra@gmail.com>
2019-12-26 00:18:08 +0530

    Added rounded border around login form, minor modification to code for selecting links in sidebar (no change in functionality).

--- client/js/index.js
+++ client/js/index.js
@@ -1,11 +1,17 @@
 const main = document.querySelector('main');
-const sidebarLinks = document.querySelectorAll('[role="sidebar"] a[href="#"]');
 
-for (const curr of sidebarLinks) 
-    curr.addEventListener('click', function() {
-        for (const link of sidebarLinks)
-            link.classList.remove('selected-link');
-        curr.classList.add('selected-link');
-        return false;           // prevent default action
-    });
+enableSidebarLinkSelection();
+
+function enableSidebarLinkSelection() {
+    const sidebarLinks = document.querySelectorAll('[role="sidebar"] a[href="#"]');
+    sidebarLinks[0].classList.add('selected-link');
+
+    for (const curr of sidebarLinks) 
+        curr.addEventListener('click', function() {
+            for (const link of sidebarLinks)
+                link.classList.remove('selected-link');
+            curr.classList.add('selected-link');
+            return false;           // prevent default action
+        });
+}
 

ae72739e9fb687b69a540086a5068119887d29ae
Sohang Chopra <sohangchopra@gmail.com>
2020-01-11 22:26:14 +0530

    The CMS Complaints link now works (just put in the link to the old form

--- client/js/index.js
+++ client/js/index.js
@@ -4,13 +4,16 @@ enableLinkSelection('.sidebar');
 
 function enableLinkSelection(elementCss) {
     const sidebarLinks = document.querySelectorAll(elementCss + ' a[href="#"]');
+    let selectedLink = sidebarLinks[0];
     sidebarLinks[0].classList.add('selected-link');
 
     for (const curr of sidebarLinks) 
         curr.addEventListener('click', function() {
-            for (const link of sidebarLinks)
-                link.classList.remove('selected-link');
-            curr.classList.add('selected-link');
+            if (curr !== selectedLink) {
+                selectedLink.classList.remove('selected-link');
+                curr.classList.add('selected-link');
+                selectedLink = curr;
+            }
             return false;           // prevent default action
         });
 }
